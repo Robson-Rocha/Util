4c7a6a62ad1aee33e7f16dc2a68064c5bafb8811
Robson Rocha - Treinamento e Consultoria <contato@robsonrocha.com.br>
2015-06-27 03:35:31 -0300

    Adicionado pacote de publicação do NuGet e informações de Assembly atualizadas; Inclusão de projeto faltante

--- Util.GeoLocation/Properties/AssemblyInfo.cs
+++ Util.GeoLocation/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RobsonROX.Util")]
-[assembly: AssemblyDescription("Bibliotecas de código utilitárias e de propósito geral")]
+[assembly: AssemblyTitle("Util.GeoLocaton")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Robson Rocha de Araújo Treinamento e Consultoria")]
-[assembly: AssemblyProduct("RobsonROX.Util")]
-[assembly: AssemblyCopyright("Copyright (c) 2015 Robson Rocha de Araújo")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Util.GeoLocaton")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a9ba778b-c5f7-4f25-8c63-4392a663ad4c")]
+[assembly: Guid("4e4056b3-2cea-4ba2-a42b-4bb185f1bcbc")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
